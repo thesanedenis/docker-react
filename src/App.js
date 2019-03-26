44cba085ac4e518803c9d4ce4a1e123100f3c17b
root <root@vm-centos7-docker02.deb-71>
2019-03-26 13:41:35 +0300

    header h2 size

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
 Russian language Русския язык !!! Some new qouic test  ----          Learn DENIS ---> and --->  Learn React
-          </a>
+          </a><br> <h2>Enather heade </h2>
         </header>
 <p>add some another text hete </p>     
 
