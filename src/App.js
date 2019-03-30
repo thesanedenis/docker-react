007f714a2b211061ea5334a18bca0255810d41d7
root <root@vm-centos7-docker02.deb-71>
2019-03-30 14:57:54 +0300

    sime commit

--- src/App.js
+++ src/App.js
@@ -16,15 +16,7 @@ class App extends Component {
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
-<<<<<<< HEAD
-Russian language Русския язык !!! Some new qouic test  ---- Learn DENIS ---and ---  Learn React
-          </a>
-<h2>Заголовок зазмера 2</h2>
-=======
-Russian language Русския язык !!! Some new qouic test  ----         Learn React
-          </a><br> <h2>----- Enather heade  ----- </h2>
->>>>>>> 9ca8a1b3e40e5b307de2c2a90320c4fd1165a44d
+          > some trouble  </a><br> <h2>----- Enather heade  ----- </h2>
         </header>
 <p>add some another text hete </p>     
 
